0a9d5b1f0432f6473ae18cbc49ebce6eae57cc00
yuhangluoEarth <115802019+yuhangluoEarth@users.noreply.github.com>
2022-10-15 11:07:33 +0800

    Delete Function directory

--- Function/gen_aff_score_Shrublands.m
+++
@@ -1,9 +0,0 @@
-function aff_score = gen_aff_score_Shrublands(single_mat_lcc,Legend,m,n)
-%m=3600;n=7200;
-aff_score = ones(m,n,2).*nan;
-for i = 1:1:m
-    single_mat_lcc_i = squeeze(single_mat_lcc(i,:,:));
-    single_mat_lcc_Trans_i = single_mat_lcc_i * Legend;
-    aff_score(i,:,:) = single_mat_lcc_Trans_i;
-end
-end
